a836524af5da721f0021786d3ab816339f551546
Sebastian Brossmer <latsch_general@web.de>
2016-08-29 12:35:40 +0200

    Upgrading Graphics class, so it can draw to the screen

--- CaveStory/src/Graphics.cpp
+++ CaveStory/src/Graphics.cpp
@@ -1,5 +1,5 @@
 #include <SDL.h>
-
+#include <SDL_image.h>
 #include "Graphics.h"
 
 
@@ -16,3 +16,26 @@ Graphics::Graphics() {
 Graphics::~Graphics() {
 	SDL_DestroyWindow(this->_window);
 };
+
+SDL_Surface* Graphics::loadImage(const std::string &filePath) {
+	if (this->_spriteSheets.count(filePath) == 0) {
+		this->_spriteSheets[filePath] = IMG_Load(filePath.c_str());
+	}
+	return _spriteSheets[filePath];
+}
+
+void Graphics::blitSurface(SDL_Texture* texture, SDL_Rect* sourceRectangle, SDL_Rect* destinaionRectangle) {
+	SDL_RenderCopy(this->_renderer, texture, sourceRectangle, destinaionRectangle);
+}
+
+void Graphics::flip() {
+	SDL_RenderPresent(this->_renderer);
+}
+
+void Graphics::clear() {
+	SDL_RenderClear(this->_renderer);
+}
+
+SDL_Renderer* Graphics::getRenderer() const {
+	return this->_renderer;
+}
